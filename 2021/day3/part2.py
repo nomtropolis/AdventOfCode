44cfb67bd5d7633c32addd9556d1d6aa306ff663
lbc <lbc@localhost.localdomain>
2021-12-13 21:02:37 -0500

    Cleaning up

--- 2021/day3/part2.py
+++ 2021/day3/part2.py
@@ -1,35 +1,7 @@
 from pprint import pprint
-def get_most_common_at_position(list_, position):
-    print("---------------------------------------in get_most_common_at_position-----------------------------------------------")
-    print("For Position: {}".format(position))
-    # counter = {0: 0, 1: 0}
-    counter = {
-        0: {
-            'items': [],
-            'count': 0
-        },
-        1: {
-            'items': [],
-            'count': 0
-        }
-    }
-    for line in list_:
-        this_item = int(line[position])
-        counter[this_item]['count'] += 1
-        counter[this_item]['items'].append(line)
-        print(counter)        
-    if counter[0]['count'] > counter[1]['count']:
-        print("---------------------------------------end get_most_common_at_position-----------------------------------------------")
-        return 0, counter
-    elif counter[1]['count'] > counter[0]['count']:
-        print("---------------------------------------end get_most_common_at_position-----------------------------------------------")
-        return 1, counter
-    else:
-        print("---------------------------------------end get_most_common_at_position-----------------------------------------------")
-        return 1, counter
+import operator
 
-def generate_matrix(filepath):
-    print("----------------------------------------------start generate_matrix-------------------------------------------------------")
+def clean_input(filepath):
     all_values = []
     with open(filepath, "r") as infile:
         for line in infile:
@@ -37,43 +9,53 @@ def generate_matrix(filepath):
             if line.strip() == "\n":
                 continue
             all_values.append(line)
-    pprint(all_values)
-    print("----------------------------------------------end generate_matrix-------------------------------------------------------")
+    
     return all_values
 
-def flip_bit(thing_to_flip):
-    if thing_to_flip == 0:
-        return 1
+def get_values(matrix, position, operand, default):
+    counter = {
+        0: {'items': [], 'count': 0},
+        1: {'items': [], 'count': 0}
+    }
+    for line in matrix:
+        this_item = int(line[position])
+        counter[this_item]['count'] += 1
+        counter[this_item]['items'].append(line)
+    if operand(counter[0]['count'], counter[1]['count']):
+        return counter[0]['items']
+    elif operand(counter[1]['count'], counter[0]['count']):
+        return counter[1]['items']
     else:
-        return 0
+        return counter[default]['items']
 
 def main(filepath):
     # oxygen generator rating most common value in the current bit position
     # co2 scrubber rating least common value in the current bit position
-    # 0110 0101 1100
-
-    all_lines = generate_matrix(filepath)
+    all_lines = clean_input(filepath)
     position = 0
-    most_common_lines = all_lines
-    least_common_lines = all_lines
     oxygen_generator_rating = None
     co2_scrubber_rating = None
-    print(all_lines)
-    while (oxygen_generator_rating is None) and (co2_scrubber_rating is None):
-        most_common_at_position, counts = get_most_common_at_position(most_common_lines, position)
-        print("For position {}, {} is the most common, there are {} items left that match the most common".format(position, most_common_at_position, len(counts[most_common_at_position]['items'])))
-        least_common_at_position = flip_bit(most_common_at_position)
-        if counts[most_common_at_position]['count'] == 1:
-            oxygen_generator_rating = counts[most_common_at_position]['items'][0]
-        if counts[least_common_at_position]['count'] == 1:
-            co2_scrubber_rating = counts[least_common_at_position]['items'][0]
-        most_common_lines = counts[most_common_at_position]['items']
+    most_common,least_common = (all_lines, all_lines)
+    while not oxygen_generator_rating or not co2_scrubber_rating:
+        if len(most_common) == 1:
+            oxygen_generator_rating = most_common[0]
+            print("Found oxygen generator rating: {}".format(oxygen_generator_rating))
+        else:
+            most_common = get_values(most_common, position, operator.gt, 1)
+
+        if len(least_common) == 1:
+            co2_scrubber_rating = least_common[0]
+            print("Found co2 scrubber rating: {}".format(co2_scrubber_rating))
+        else:
+            least_common = get_values(least_common, position, operator.lt, 0)
         position += 1
-    print(oxygen_generator_rating)
-    answer = int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2)
-    print("Answer is: {}".format(answer))
+    print("Answer is: {} * {} = {}".format(
+        int(oxygen_generator_rating, 2),
+        int(co2_scrubber_rating, 2),
+        int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2)        
+    ))
 
 if __name__ == "__main__":
-    file_to_load = "test_input.txt"
-    # file_to_load = "input.txt"
+    # file_to_load = "test_input.txt"
+    file_to_load = "input.txt"
     main(file_to_load)
